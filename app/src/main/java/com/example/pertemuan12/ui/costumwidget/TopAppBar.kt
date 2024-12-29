fcace820f51d1f224d807a353df01c6b9808aed9
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 11:56:34 +0700

    Melengkapi code centeralignedTopAppbar

--- app/src/main/java/com/example/pertemuan12/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/costumwidget/TopAppBar.kt
@@ -2,10 +2,12 @@ package com.example.pertemuan12.ui.customwidget
 
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,6 +29,14 @@ fun CustomTopAppBar(
             Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable{
                 onRefresh()
             })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
         }
     )
 }
