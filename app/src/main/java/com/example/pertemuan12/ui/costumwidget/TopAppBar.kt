b924683542824db66e4d04488043ced345aae5a4
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 11:53:34 +0700

    Membuat CenterAlignedTopAppbar

--- app/src/main/java/com/example/pertemuan12/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/costumwidget/TopAppBar.kt
@@ -1,6 +1,12 @@
 package com.example.pertemuan12.ui.customwidget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -15,5 +21,12 @@ fun CustomTopAppBar(
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
 ) {
-
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable{
+                onRefresh()
+            })
+        }
+    )
 }
