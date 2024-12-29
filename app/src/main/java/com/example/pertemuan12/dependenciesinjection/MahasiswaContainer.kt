1d3e6c3b9607ff062d6e67f380f562c4db3c1766
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 10:39:43 +0700

    memperbaiki eror toMediaType

--- app/src/main/java/com/example/pertemuan12/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12/dependenciesinjection/MahasiswaContainer.kt
@@ -9,23 +9,23 @@ import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.create
 
-interface AppContainer{
+interface AppContainer {
     val kontakRepository: MahasiswaRepository
 }
 
-class MahasiswaContainer : AppContainer{
+class MahasiswaContainer : AppContainer {
 
     private val baseUrl = "http://10.0.2.2:8000/umyTI/"
-    private val json = Json{ignoreUnknownKeys = true}
+    private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType))
-        .baseUrl(baseUrl).build()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
 
-    private val MahasiswaService: MahasiswaService by lazy {
+    private val mahasiswaService: MahasiswaService by lazy {
         retrofit.create(MahasiswaService::class.java)
     }
     override val kontakRepository: MahasiswaRepository by lazy {
-        NetworkKontakRepository(MahasiswaService)
+        NetworkKontakRepository(mahasiswaService)
     }
-
 }
