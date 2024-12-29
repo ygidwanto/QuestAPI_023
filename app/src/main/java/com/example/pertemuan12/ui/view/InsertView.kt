083802b31fa6de1703ee4988566116fd23dd656c
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 12:40:08 +0700

    Melengkapi insertview

--- app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
@@ -1,11 +1,23 @@
 package com.example.pertemuan12.ui.view
 
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.navigation.DestinasiNavigasi
 import com.example.pertemuan12.ui.viewmodel.InsertUiEvent
@@ -64,6 +76,97 @@ fun EntryBody(
     onSaveClick: () -> Unit,
     modifier: Modifier= Modifier
 ){
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Simpan")
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier= Modifier,
+    onValueChange:(InsertUiEvent)-> Unit={},
+    enabled: Boolean=true
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nim,
+            onValueChange = { onValueChange(insertUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertUiEvent.jenisKelamin,
+            onValueChange = { onValueChange(insertUiEvent.copy(jenisKelamin = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = { onValueChange(insertUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertUiEvent.kelas,
+            onValueChange = { onValueChange(insertUiEvent.copy(jenisKelamin = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.angkatan,
+            onValueChange = { onValueChange(insertUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "Isi Semua Data",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
