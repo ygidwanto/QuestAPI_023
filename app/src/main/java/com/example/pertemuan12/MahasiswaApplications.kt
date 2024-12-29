abcf23af8dbd2414c7e3e07593046bc8d124311c
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 10:44:20 +0700

    Membuas MahasiswaApplications

---
+++ app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan12
+
+import android.app.Application
+import com.example.pertemuan12.dependenciesinjection.AppContainer
+import com.example.pertemuan12.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApplications: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
