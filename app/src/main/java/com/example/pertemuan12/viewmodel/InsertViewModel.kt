92d30b863add1f556342f9335fcbb20c0d2a1aed
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 11:02:32 +0700

    Membuat InsertViewModel

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent){
+        uiState=InsertUiState(insertUievent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch{
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
