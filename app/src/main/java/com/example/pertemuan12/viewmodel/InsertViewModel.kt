b6ecadf94a8a6598f580a1a0c11fcebd9e1345b7
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 11:13:48 +0700

    Membuat data class InsertUiEvent

--- app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
@@ -27,3 +27,12 @@ class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel() {
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
+
+data class InsertUiEvent(
+    val nim: String="",
+    val nama: String="",
+    val alamat: String= "",
+    val jenisKelamin: String= "",
+    val kelas: String= "",
+    val angkatan: String = ""
+)
