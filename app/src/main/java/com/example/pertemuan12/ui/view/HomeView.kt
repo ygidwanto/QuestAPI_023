a68c231571d72fbce36133f75e7c378119b7bf16
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 14:44:22 +0700

    Membuat home uistate

--- app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/HomeView.kt
@@ -1,19 +1,25 @@
 package com.example.pertemuan12.ui.view
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.navigation.DestinasiNavigasi
+import com.example.pertemuan12.ui.viewmodel.HomeUiState
 import com.example.pertemuan12.ui.viewmodel.HomeViewModel
 import com.example.pertemuan12.ui.viewmodel.PenyediaViewModel
 import java.lang.reflect.Modifier
@@ -62,5 +68,29 @@ fun HomeScreen(
             }
         )
 
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction:()-> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick:(Mahasiswa)-> Unit={},
+    onDetailClick: (String) -> Unit
+){
+    when(HomeUiState){
+        is HomeUiState.Loading->OnLoading(modifier=modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.mahasiswa.isEmpty()){
+                return Box(modifier=modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data Kontak")
+                }
+            }else{
+                MhsLayout(
+
+                )
+            }
     }
 }
