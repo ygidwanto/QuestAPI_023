8be4ef8d1d4201a132722829b811b53431cfee58
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-12-29 11:18:42 +0700

    Membuat fun mahasiswa, insert ui event

--- app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/InsertViewModel.kt
@@ -41,3 +41,12 @@ data class InsertUiEvent(
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
